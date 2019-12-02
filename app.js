329dce9d9a3705b2262a57c0a0034c787022bbce
Palodiuss <palodiuss@gmail.com>
2019-12-02 23:51:56 +0100

    pixel perfect fixes

--- app.js
+++ app.js
@@ -1,6 +1,48 @@
 document.querySelector('.navicon').addEventListener('click', (event)=>{
-    event.target.classList.toggle('navicon-close');
-   document.querySelector('.nav-links').classList.toggle('hidden');
-   document.querySelector('.pl').classList.toggle('hidden');
+    document.querySelector('.navicon').classList.toggle('open');
+    document.querySelector('.nav-links').classList.toggle('hidden');
+    
+
+    document.querySelector('.language-menu').classList.toggle('hidden');
 });
 
+
+let open = false;
+let language = 'opt1';
+
+
+document.querySelector('.language-menu').addEventListener('click', (event)=>{
+    event.target.closest('.language-menu').classList.toggle('pl-open');
+    document.querySelector('.pl-arrow').classList.toggle('arrow-rotate');
+ 
+
+    if (open===true){
+        if(event.target.parentNode.id) {
+            document.getElementById('opt1').classList.add('option-hidden');
+            document.getElementById('opt2').classList.add('option-hidden');
+
+            document.getElementById(event.target.parentNode.id).classList.remove('option-hidden');
+            language = event.target.parentNode.id;
+
+
+        }
+        else {
+            document.getElementById('opt1').classList.add('option-hidden');
+            document.getElementById('opt2').classList.add('option-hidden');
+            document.getElementById(language).classList.remove('option-hidden');
+        }
+    }
+    else {
+        document.getElementById('opt1').classList.remove('option-hidden');
+        document.getElementById('opt2').classList.remove('option-hidden');
+    }
+
+    open = !open;
+
+})
+
+
+
+// name-label
+
+//form-name
